4720a52b3e6ddf53c7a6ca0df6ab7c9d7707cc8e
KOTOKINEW <mazaika1997@mail.ru>
2021-02-11 19:56:34 +0300

    Initial commit

---
+++ src/main/java/com/kotoki/stud/test_project/TestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kotoki.stud.test_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectApplication.class, args);
+	}
+
+}
