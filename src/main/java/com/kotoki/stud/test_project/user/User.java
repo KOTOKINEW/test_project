3f90959bed5e01e3d5dfece64feb4774ba1e8852
KOTOKINEW <mazaika1997@mail.ru>
2021-03-12 19:39:29 +0300

    #13 and #14

--- src/main/java/com/kotoki/stud/test_project/user/User.java
+++ src/main/java/com/kotoki/stud/test_project/user/User.java
@@ -1,7 +1,12 @@
 package com.kotoki.stud.test_project.user;
 
 import lombok.Data;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "user", schema = "db_schema")
